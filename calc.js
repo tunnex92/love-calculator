90b56fabb42f49f86deecfbd9891e9440decf88b
tunnex92 <tunnex92@gmail.com>
2022-04-25 05:32:43 +0100

    trial

---
+++ calc.js
@@ -0,0 +1,16 @@
+var firstinput = prompt("Input your name");
+var secondinput = prompt("Input your partner name");
+function loveCalculator (Mary, John) {
+    var lovecomp = (Math.random() * 100pwd) + 1;
+    var result = Math.floor(lovecomp);
+alert(firstinput + 
+" your love compatibility with " + secondinput + 
+" is " + result + "% ");
+if (result < 50) {
+    result = "Stay away from each other, you are not compactible"
+}
+// return("you got the game, " 
+// + firstinput + " your love compatibility with " + secondinput + " is " + result + "% ");
+}
+loveCalculator(firstinput, secondinput)
+
